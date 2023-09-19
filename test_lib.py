ba6022e7c5a692b229dd7525ea5ad21f44ce4d73
JiayiZhou <jiayizhou@JiayiZhous-MacBook-Pro.local>
2023-09-19 13:15:37 -0400

    modified

--- test_lib.py
+++ test_lib.py
@@ -11,13 +11,16 @@ def test_read():
         "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
     )
     assert isinstance(df1, pd.DataFrame), "Fail to read the csv file"
+    return df1
 
 
 def test_describe():
     """test function for describe dataframe"""
     # read a csv file by url
     df1 = describe_file(
-        "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+        read_file(
+            "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+        )
     )
     assert df1[["describe", "year"]][4, 1] == 1921.0
     assert df1[["describe", "ppf"]][0, 1] == 30962.0
@@ -27,6 +30,8 @@ def test_describe():
 def test_plot():
     """test function for plot"""
     plot_file(
-        "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+        read_file(
+            "https://raw.githubusercontent.com/fivethirtyeight/data/master/goose/goose_rawdata.csv"
+        )
     )
     assert True, "Plot generation successed"
